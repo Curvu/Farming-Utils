d90a6902b083184a26e1f7b7ac45e8501ce0f1ea
Curvu <curvu6@gmail.com>
2023-07-06 09:45:32 +0100

    + Crops when full-grown have a full hit-box, so it's easy to break them

---
+++ src/main/java/com/curvu/farmingutils/mixins/CropsMixin.java
@@ -0,0 +1,28 @@
+package com.curvu.farmingutils.mixins;
+
+
+import com.curvu.farmingutils.render.CropsHitBox;
+import net.minecraft.block.BlockBush;
+import net.minecraft.block.BlockCrops;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(BlockCrops.class)
+public abstract class CropsMixin extends BlockBush {
+
+  @Override
+  public AxisAlignedBB getSelectedBoundingBox(World worldIn, BlockPos pos) {
+    CropsHitBox.updateCropsMaxY(worldIn, pos, worldIn.getBlockState(pos).getBlock());
+    return super.getSelectedBoundingBox(worldIn, pos);
+  }
+
+  @Override
+  public MovingObjectPosition collisionRayTrace(World worldIn, BlockPos pos, Vec3 start, Vec3 end) {
+    CropsHitBox.updateCropsMaxY(worldIn, pos, worldIn.getBlockState(pos).getBlock());
+    return super.collisionRayTrace(worldIn, pos, start, end);
+  }
+}
