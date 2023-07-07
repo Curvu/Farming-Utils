734fced458b1cfa9f17233808df36abc04777089
Curvu <curvu6@gmail.com>
2023-07-07 10:26:10 +0100

    mushrooms now have hitbox

--- src/main/java/com/curvu/farmingutils/FarmingUtils.java
+++ src/main/java/com/curvu/farmingutils/FarmingUtils.java
@@ -4,6 +4,8 @@ import com.curvu.farmingutils.listeners.FeatureListener;
 import net.minecraft.block.*;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -13,11 +15,13 @@ import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import java.lang.reflect.Field;
+
 @Mod(modid = FarmingUtils.MODID, name = FarmingUtils.NAME, version = FarmingUtils.VERSION)
 public class FarmingUtils {
   public static final String MODID = "farmingutils";
   public static final String NAME = "FarmingUtils";
-  public static final String VERSION = "1.1.0";
+  public static final String VERSION = "1.1.2";
 
   @Mod.Instance
   public static FarmingUtils main;
@@ -39,10 +43,13 @@ public class FarmingUtils {
       age = BlockCrops.AGE;
     }
 
-    //This shouldn't happen, but just in case
     if(age == null) return;
 
-    ObfuscationReflectionHelper.setPrivateValue(Block.class, block, heights[blockState.getValue(age)], "maxY", "field_149756_F");
+    try {
+      ObfuscationReflectionHelper.setPrivateValue(Block.class, block, heights[blockState.getValue(age)], "maxY", "field_149756_F");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @EventHandler
@@ -54,5 +61,8 @@ public class FarmingUtils {
   public void init(FMLInitializationEvent event) {
     MinecraftForge.EVENT_BUS.register(this);
     MinecraftForge.EVENT_BUS.register(new FeatureListener());
+
+    Blocks.brown_mushroom.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
+    Blocks.red_mushroom.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, 1.0F, 1.0F);
   }
 }
