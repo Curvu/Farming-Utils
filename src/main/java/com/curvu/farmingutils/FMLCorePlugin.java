945167fac4ca4c4be15b640d62fa5c717a5da5d0
Curvu <curvu6@gmail.com>
2023-07-13 09:51:06 +0100

    added mixins

--- src/main/java/com/curvu/farmingutils/FMLCorePlugin.java
+++ src/main/java/com/curvu/farmingutils/FMLCorePlugin.java
@@ -2,11 +2,21 @@ package com.curvu.farmingutils;
 
 import com.curvu.farmingutils.misc.ClassTransformer;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.MixinEnvironment;
+import org.spongepowered.asm.mixin.Mixins;
 
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.8.9")
 public class FMLCorePlugin implements IFMLLoadingPlugin {
+
+  // Add the mixin configuration here
+  public FMLCorePlugin() {
+    MixinBootstrap.init();
+    Mixins.addConfiguration("mixins.farmingutils.json");
+    MixinEnvironment.getDefaultEnvironment().setSide(MixinEnvironment.Side.CLIENT);
+  }
 
   @Override
   public String[] getASMTransformerClass() {
